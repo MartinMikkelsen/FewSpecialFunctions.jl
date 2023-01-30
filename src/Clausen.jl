c32e68a3b493510bc1f788ab6ad263a6b8762bf4
MartinMikkelsen <201706771@post.au.dk>
2023-01-30 10:53:07 +0100

    docstring

--- src/Clausen.jl
+++ src/Clausen.jl
@@ -1,5 +1,10 @@
-"""math
+"""
+Function which returns the value of the Clausen funtion given by
+
+```math
     C_l(\phi) = - \int_0^\phi \log(|2\sin(x/2)|) dx
+```
+See https://en.wikipedia.org/wiki/Clausen_function
 """
 function Clausen(x, min_tol=1e-15)
     return real((-quadgk(t -> log(2*Complex(sin(t/2))),min_tol,x)[1]))
