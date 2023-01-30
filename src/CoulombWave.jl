728c500e3915ca92086e5b533e787a4d5275300d
MartinMikkelsen <201706771@post.au.dk>
2023-01-30 10:58:15 +0100

    docstring

--- src/CoulombWave.jl
+++ src/CoulombWave.jl
@@ -1,7 +1,8 @@
 export complex_quadrature, regular_coulomb, irregular_coulomb, C
 
-using SpecialFunctions
+using SpecialFunctions #For gamma function
 
+"Defines a cool function. Returns some stuff"
 function regular_coulomb(ℓ,η,ρ)
     First = ρ.^(ℓ+1)*2^ℓ*exp(1im.*ρ-(π.*η/2))/(abs(gamma(ℓ+1+1im*η)))
     Integral_value = complex_quadrature(t -> exp(-2*1im.*ρ*t).*t.^(ℓ+1im*η)*(1-t).^(ℓ-1im*η),1e-5,1)
