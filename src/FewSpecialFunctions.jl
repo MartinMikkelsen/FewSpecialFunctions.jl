79d6864152da99b7b89276bb842e5782d98d5aee
MartinMikkelsen <201706771@post.au.dk>
2023-02-07 11:50:00 +0100

    toml

--- src/FewSpecialFunctions.jl
+++ src/FewSpecialFunctions.jl
@@ -9,7 +9,7 @@ include("CoulombWave.jl")
 export Debye_function
 include("Debye.jl")
 
-export Fresnel_S_integral_pi, Fresnel_C_integral_pi , Fresnel_S_integral, Fresnel_C_integral , Fresnel_S_err, Fresnel_C_err
+export Fresnel_S_integral_pi, Fresnel_C_integral_pi , Fresnel_S_integral, Fresnel_C_integral , Fresnel_S_erf, Fresnel_C_erf
 include("Fresnel.jl")
 
 export Struve
