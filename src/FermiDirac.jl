cc07169e7041d9ae1ae99d5f4c12f4a26459a8e3
MartinMikkelsen <201706771@post.au.dk>
2023-03-24 12:37:29 +0100

    Fermi-Dirac integrals

--- src/FermiDirac.jl
+++ src/FermiDirac.jl
@@ -14,20 +14,45 @@ The Fermi-Dirac integral
 Returns the value ``F_j(x)``
 
 Resources:
-
+https://arxiv.org/abs/0811.0116
 https://de.wikipedia.org/wiki/Fermi-Dirac-Integral
+https://dlmf.nist.gov/25.12#iii
+
 """
-function FermiDiracIntegral(j,x)
-    if j==0
-        return log(1+exp(x))
+function FermiDiracIntegral(j::Real, eta::Real)
+    if j < -1 / 2
+        error("The order should be equal to or larger than -1/2.")
+    else
+        x = eta
+        y = zero(x)
+        if j == 0
+            y = log(1 + exp(x))
+        elseif j == 1 / 2
+            # Model proposed in [1]
+            # Expressions from eqs. (22)-(24) of [2]
+            mu = x^4 + 50 + 33.6 * x * (1 - 0.68 * exp(-0.17 * (x + 1)^2))
+            xi = 3 * sqrt(pi) / (4 * mu^(3 / 8))
+            y = (exp(-x) + xi)^-1
+        elseif j == 3 / 2
+            # Model proposed in [3]
+            # Expressions from eq. (5) of [3]
+            # The integral is divided by gamma(j + 1) to make it consistent with [1] and [2].
+            a = 14.9
+            b = 2.64
+            c = 9 / 4
+            y = ((j + 1) * 2^(j + 1) / (b + x + (abs(x - b).^ c + a) .^ (1 / c)) .^ (j + 1) +
+                 exp(-x) / gamma(j + 1))^-1 / gamma(j + 1)
+        else
+            # Model proposed in [4]
+            # Expressions from eqs. (6)-(7) of [4]
+            # The integral is divided by gamma(j + 1) to make it consistent with [1] and [2].
+            a = (1 + 15 / 4 * (j + 1) + 1 / 40 * (j + 1)^2)^(1 / 2)
+            b = 1.8 + 0.61 * j
+            c = 2 + (2 - sqrt(2)) * 2^(-j)
+            y = ((j + 1) * 2^(j + 1) / (b + x + (abs(x - b) .^ c + a^c) .^ (1 / c)) .^ (j + 1) +
+                 exp(-x) / gamma(j + 1))^-1 / gamma(j + 1)
+        end
     end
-    if j==-1
-        return exp(x)/(1+exp(x))
-    end
-    if j==1/2 && x<1.3
-        return  1/(exp(-x)+0.27)
-    end
-    if j==1/2 && x>=1.3 
-        return 4/(3*sqrt(π))*(x^2+π^2/6)^(3/4)
-    end 
+    return y
 end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,51 +3,79 @@ using Test
 
 @testset "FewSpecialFunctions.jl" begin
 
-    @test FewSpecialFunctions.regular_coulomb(0.0,0.3,1e-6) ≈ 0.0 atol=1e-5
-    @test FewSpecialFunctions.regular_coulomb(0.0,-0.3,1e-6) ≈ 0.0 atol=1e-5
-    @test FewSpecialFunctions.regular_coulomb(0.0,2.0,5.0) ≈ 1.1433 atol=1e-4
-    @test FewSpecialFunctions.regular_coulomb(1.0,2.0,5.0) ≈ 0.99347 atol=1e-4
-    @test FewSpecialFunctions.regular_coulomb(2.0,2.0,5.0) ≈ 0.72125 atol=1e-4
-    @test FewSpecialFunctions.regular_coulomb(3.0,2.0,5.0) ≈ 0.4313 atol=1e-4
+    @test FewSpecialFunctions.regular_Coulomb(0.0, 0.3, 1e-6) ≈ 0.0 atol = 1e-5
+    @test FewSpecialFunctions.regular_Coulomb(0.0, -0.3, 1e-6) ≈ 0.0 atol = 1e-5
+    @test FewSpecialFunctions.regular_Coulomb(0.0, 2.0, 5.0) ≈ 1.1433 atol = 1e-4
+    @test FewSpecialFunctions.regular_Coulomb(1.0, 2.0, 5.0) ≈ 0.99347 atol = 1e-4
+    @test FewSpecialFunctions.regular_Coulomb(2.0, 2.0, 5.0) ≈ 0.72125 atol = 1e-4
+    @test FewSpecialFunctions.regular_Coulomb(3.0, 2.0, 5.0) ≈ 0.4313 atol = 1e-4
 
-    @test FewSpecialFunctions.Debye_function(1.0,0.1) ≈ 0.975278 atol=1e-3
-    @test FewSpecialFunctions.Debye_function(1.0,0.2) ≈ 0.951111 atol=1e-4
+    @test FewSpecialFunctions.Debye_function(1.0, 0.1) ≈ 0.975278 atol = 1e-3
+    @test FewSpecialFunctions.Debye_function(1.0, 0.2) ≈ 0.951111 atol = 1e-4
 
-    @test FewSpecialFunctions.Debye_function(1.0,1.0) ≈ 0.777505 atol=1e-4
-    @test FewSpecialFunctions.Debye_function(1.0,1.3) ≈ 0.721173 atol=1e-4
+    @test FewSpecialFunctions.Debye_function(1.0, 1.0) ≈ 0.777505 atol = 1e-4
+    @test FewSpecialFunctions.Debye_function(1.0, 1.3) ≈ 0.721173 atol = 1e-4
 
-    @test FewSpecialFunctions.Debye_function(1.0,2.6) ≈ 0.526375 atol=1e-4
-    @test FewSpecialFunctions.Debye_function(1.0,2.8) ≈ 0.502682 atol=1e-4
- 
-    @test FewSpecialFunctions.Debye_function(2.0,1.3) ≈ 0.635800 atol=1e-4
-    @test FewSpecialFunctions.Debye_function(2.0,2.8) ≈ 0.368324 atol=1e-4
+    @test FewSpecialFunctions.Debye_function(1.0, 2.6) ≈ 0.526375 atol = 1e-4
+    @test FewSpecialFunctions.Debye_function(1.0, 2.8) ≈ 0.502682 atol = 1e-4
 
-    @test FewSpecialFunctions.Debye_function(3.0,0.7) ≈ 0.761859 atol=1e-4
-    @test FewSpecialFunctions.Debye_function(3.0,2.8) ≈ 0.309995 atol=1e-4
+    @test FewSpecialFunctions.Debye_function(2.0, 1.3) ≈ 0.635800 atol = 1e-4
+    @test FewSpecialFunctions.Debye_function(2.0, 2.8) ≈ 0.368324 atol = 1e-4
 
-    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.10) ≈ 0.0005236 atol=1e-4
-    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.32) ≈ 0.0171256 atol=1e-4
-    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.70) ≈ 0.1721365 atol=1e-4
+    @test FewSpecialFunctions.Debye_function(3.0, 0.7) ≈ 0.761859 atol = 1e-4
+    @test FewSpecialFunctions.Debye_function(3.0, 2.8) ≈ 0.309995 atol = 1e-4
 
-    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.10) ≈ 0.0999975 atol=1e-4
-    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.32) ≈ 0.3191731 atol=1e-4
-    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.70) ≈ 0.6596524 atol=1e-4
+    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.10) ≈ 0.0005236 atol = 1e-4
+    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.32) ≈ 0.0171256 atol = 1e-4
+    @test FewSpecialFunctions.Fresnel_S_integral_pi(0.70) ≈ 0.1721365 atol = 1e-4
 
-    @test FewSpecialFunctions.Fresnel_S_err(0.10) ≈ FewSpecialFunctions.Fresnel_S_integral(0.10) atol=1e-5
-    @test FewSpecialFunctions.Fresnel_S_err(0.32) ≈ FewSpecialFunctions.Fresnel_S_integral(0.32) atol=1e-5
-    @test FewSpecialFunctions.Fresnel_S_err(0.70) ≈ FewSpecialFunctions.Fresnel_S_integral(0.70) atol=1e-5
+    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.10) ≈ 0.0999975 atol = 1e-4
+    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.32) ≈ 0.3191731 atol = 1e-4
+    @test FewSpecialFunctions.Fresnel_C_integral_pi(0.70) ≈ 0.6596524 atol = 1e-4
 
-    @test FewSpecialFunctions.Fresnel_C_err(0.10) ≈ FewSpecialFunctions.Fresnel_C_integral(0.10) atol=1e-5
-    @test FewSpecialFunctions.Fresnel_C_err(0.32) ≈ FewSpecialFunctions.Fresnel_C_integral(0.32) atol=1e-5
-    @test FewSpecialFunctions.Fresnel_C_err(0.70) ≈ FewSpecialFunctions.Fresnel_C_integral(0.70) atol=1e-5
+    @test FewSpecialFunctions.Fresnel_S_erf(0.10) ≈ FewSpecialFunctions.Fresnel_S_integral(0.10) atol = 1e-5
+    @test FewSpecialFunctions.Fresnel_S_erf(0.32) ≈ FewSpecialFunctions.Fresnel_S_integral(0.32) atol = 1e-5
+    @test FewSpecialFunctions.Fresnel_S_erf(0.70) ≈ FewSpecialFunctions.Fresnel_S_integral(0.70) atol = 1e-5
+
+    @test FewSpecialFunctions.Fresnel_C_erf(0.10) ≈ FewSpecialFunctions.Fresnel_C_integral(0.10) atol = 1e-5
+    @test FewSpecialFunctions.Fresnel_C_erf(0.32) ≈ FewSpecialFunctions.Fresnel_C_integral(0.32) atol = 1e-5
+    @test FewSpecialFunctions.Fresnel_C_erf(0.70) ≈ FewSpecialFunctions.Fresnel_C_integral(0.70) atol = 1e-5
 
 
-    @test FewSpecialFunctions.Struve(0.0,4) ≈ 0.1350146 atol=1e-4
-    @test FewSpecialFunctions.Struve(1.0,4) ≈ 1.0697267 atol=1e-4
-    @test FewSpecialFunctions.Struve(2.0,4) ≈ 1.2486751 atol=1e-4
+    @test FewSpecialFunctions.Struve(0.0, 4) ≈ 0.1350146 atol = 1e-4
+    @test FewSpecialFunctions.Struve(1.0, 4) ≈ 1.0697267 atol = 1e-4
+    @test FewSpecialFunctions.Struve(2.0, 4) ≈ 1.2486751 atol = 1e-4
 
-    @test FewSpecialFunctions.Clausen(0) ≈ 0 atol=1e-4
-    @test FewSpecialFunctions.Clausen(π/3+2*π) ≈ 1.01494160 atol=1e-4
-    @test FewSpecialFunctions.Clausen(-π/3+2*π) ≈ -1.01494160 atol=1e-4
+    @test FewSpecialFunctions.Clausen(0) ≈ 0 atol = 1e-4
+    @test FewSpecialFunctions.Clausen(π / 3 + 2 * π) ≈ 1.01494160 atol = 1e-4
+    @test FewSpecialFunctions.Clausen(-π / 3 + 2 * π) ≈ -1.01494160 atol = 1e-4
 
+    # Test the output for j = 0 and various values of x
+    #@test FewSpecialFunctions.FermiDiracIntegral(0, 0) ≈ 0.6931471805599453
+    #@test FewSpecialFunctions.FermiDiracIntegral(0, 1) ≈ 1.313261687518223
+    #@test FewSpecialFunctions.FermiDiracIntegral(0, 2) ≈ 2.1269280110429727
+
+    # Test the output for j = -1 and various values of x
+    #@test FewSpecialFunctions.FermiDiracIntegral(-1, 0) ≈ 0.5
+    #@test FewSpecialFunctions.FermiDiracIntegral(-1, 1) ≈ 0.7310585786300049
+    #@test FewSpecialFunctions.FermiDiracIntegral(-1, 2) ≈ 0.8807970779778823
+
+    # Test the output for j = 1/2 and x < 1.3
+    @test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 0) ≈ 2/(sqrt(π))*0.6780938951530457 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 1) ≈ 2/(sqrt(π))*1.3963752806666279 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 1.2) ≈ 2/(sqrt(π))*1.5863233997463857 atol = 1e-2
+
+    # Test the output for j = 1/2 and x >= 1.3
+    #@test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 1.3) ≈ 2/(sqrt(π))*1.0056522092269615
+    #@test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 2) ≈ 2/(sqrt(π))*1.6678511889373662
+    #@test FewSpecialFunctions.FermiDiracIntegral(1 / 2, 3) ≈ 2/(sqrt(π))*2.6101147713212985
+
+    # Test the output for j = 3/2
+    @test FewSpecialFunctions.FermiDiracIntegral(3 / 2, 0.0) ≈ 4/(3*sqrt(π))*1.15280383708879 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(3 / 2, 1) ≈ 4/(3*sqrt(π))*2.6616826247307124 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(3 / 2, 1.2) ≈ 4/(3*sqrt(π))*3.10869199517456 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(3 / 2, -2.5) ≈ 4/(3*sqrt(π))*0.1075808743944384 atol = 1e-2
+    @test FewSpecialFunctions.FermiDiracIntegral(3 / 2, -2.0) ≈ 4/(3*sqrt(π))*0.175800988853926 atol = 1e-2
+
+    #for some reason the tolerance must be high?
 end
