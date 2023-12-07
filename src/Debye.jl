15803f77df5ab1454b64fea67304609bdd91d726
MartinMikkelsen <201706771@post.au.dk>
2023-12-07 09:43:42 +0100

    typo

--- src/Debye.jl
+++ src/Debye.jl
@@ -4,7 +4,7 @@
 The Debye function(n,x) given by
     
 ```math
-    D_n(x) = \frac{n}{x^n} \int_0^x \frac{t^n}{e^{t}-1} dx
+    D_n(x) = \frac{n}{x^n} \int_0^x \frac{t^n}{e^{t}-1} dt
 ```
 
 Returns the value ``D(n,x)``
