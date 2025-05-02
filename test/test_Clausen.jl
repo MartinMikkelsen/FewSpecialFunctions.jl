da93658582200d18c73a14fa07c2ea4a60af7ce0
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-05-02 17:53:05 +0200

    added Clausen1

--- test/test_Clausen.jl
+++ test/test_Clausen.jl
@@ -1,7 +1,23 @@
+using DelimitedFiles
+
 @testset "Clausen" begin
-    
-    @test FewSpecialFunctions.Clausen(0) ≈ 0 atol = 1e-4
-    @test FewSpecialFunctions.Clausen(π / 3 + 2 * π) ≈ 1.01494160 atol = 1e-4
-    @test FewSpecialFunctions.Clausen(-π / 3 + 2 * π) ≈ -1.01494160 atol = 1e-4
+
+    data = open(readdlm, joinpath(@__DIR__, "data", "Cl1.txt"))
+
+    for r in 1:size(data, 1)
+        row      = data[r, :]
+        x        = row[1]
+        expected = row[2]
+
+        @test FewSpecialFunctions.Clausen(1,x) ≈ expected rtol=1e-13
+        @test FewSpecialFunctions.Clausen(1,-x) ≈ expected rtol=1e-13
+        @test FewSpecialFunctions.Clausen(1,x-2pi) ≈ expected rtol=1e-12
+        @test FewSpecialFunctions.Clausen(1,x+2pi) ≈ expected rtol=1e-13
+
+    end
+
+    @test FewSpecialFunctions.Clausen(1,0.5) ≈ 0.70358563513784466 rtol=1e-14
+    @test FewSpecialFunctions.Clausen(1,1.0) ≈ 0.042019505825368962 rtol=1e-14
+    @test FewSpecialFunctions.Clausen(1,2.0) ≈ -0.52054343429085363 rtol=1e-14
 
 end
