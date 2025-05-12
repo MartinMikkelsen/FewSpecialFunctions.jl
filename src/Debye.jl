41a403dfb688fdd6dfb53bfff1b78d66289f3fa1
MartinMikkelsen <MartinMikkelsen@protonmail.com>
2025-05-12 16:06:51 +0200

    updated tests for Debye functions

--- src/Debye.jl
+++ src/Debye.jl
@@ -1,16 +1,37 @@
-@doc raw"""
-    Debye_function(n,x,min_tol=1e-15)
+using SpecialFunctions
 
-The Debye function(n,x) given by
-    
-```math
-    D_n(x) = \frac{n}{x^n} \int_0^x \frac{t^n}{e^{t}-1} dt
-```
+export debye_function
 
-Returns the value ``D(n,x)``
 """
-function Debye_function(n,x,min_tol=1e-5)
-    D = n./(x.^n).*quadgk(t -> (t.^n)/(expm1.(t)),min_tol,x)[1]
-    return D    
+    debye_function(n::Float64, β::Float64, x::Float64; 
+                  tol=1e-35, max_terms=2000) -> Float64
+
+Compute the generalized Debye function with parameters `n`, `β`, and `x`.
+
+References:
+- [Debye function](https://en.wikipedia.org/wiki/Debye_function)    
+- [Calculation of Integer and Noninteger n-Dimensional Debye Functions Using Binomial Coefficients and Incomplete Gamma Functions](10.1007/s10765-007-0256-1)
+"""
+function debye_function(n::Float64, β::Float64, x::Float64; tol=1e-35, max_terms=2000)
+    sum = 0.0
+    c = 1.0 
+
+    γn1 = gamma(n + 1) 
+    @inbounds for i in 0:max_terms
+        ψ = β + i
+        P, _ = gamma_inc(n + 1, ψ * x) 
+        γlower = P * γn1
+
+        term = c * γlower / ψ^(n + 1)
+        sum += term
+
+        if abs(term) < tol * abs(sum)
+            return n * sum / x^n
+        end
+
+        c *= (β + i) / (i + 1)
+    end
+
+    return n * sum / x^n
 end
-export Debye_function
+
--- test/test_Debye.jl
+++ test/test_Debye.jl
@@ -1,16 +1,25 @@
+using DelimitedFiles
+using Test
+
 @testset "Debye" begin
-    @test FewSpecialFunctions.Debye_function(1.0, 0.1) ≈ 0.975278 atol = 1e-3
-    @test FewSpecialFunctions.Debye_function(1.0, 0.2) ≈ 0.951111 atol = 1e-4
 
-    @test FewSpecialFunctions.Debye_function(1.0, 1.0) ≈ 0.777505 atol = 1e-4
-    @test FewSpecialFunctions.Debye_function(1.0, 1.3) ≈ 0.721173 atol = 1e-5
+    data = open(readdlm, joinpath(@__DIR__, "data", "debye_test.txt"))
 
-    @test FewSpecialFunctions.Debye_function(1.0, 2.6) ≈ 0.526375 atol = 1e-5
-    @test FewSpecialFunctions.Debye_function(1.0, 2.8) ≈ 0.502682 atol = 1e-5
+    @testset "Debye_function vs DebyeFunctions.jl" begin
+        for r in 1:size(data, 1)
+            x, n, Q_ref = data[r, :]
+            @test FewSpecialFunctions.debye_function(n, 1.0, x) ≈ Q_ref atol= 1e-4
+        end
+    end
 
-    @test FewSpecialFunctions.Debye_function(2.0, 1.3) ≈ 0.635800 atol = 1e-5
-    @test FewSpecialFunctions.Debye_function(2.0, 2.8) ≈ 0.368324 atol = 1e-5
+    @test FewSpecialFunctions.debye_function(2.0, 1.0, 5.0) ≈ 0.172329034857624782145 atol=1e-6
+    @test FewSpecialFunctions.debye_function(5.0, 1.0, 4.5) ≈ 0.10164118339698890968 atol=1e-15
+    @test FewSpecialFunctions.debye_function(7.0, 1.0, 0.8) ≈ 0.69112406526865230673 atol=1e-14
+    @test FewSpecialFunctions.debye_function(9.0, 1.0, 3.4) ≈ 0.15413773867789254146 atol=1e-15
+    @test FewSpecialFunctions.debye_function(12.0, 1.0, 5.4) ≈ 0.03618849233828133 atol=1e-15
+    @test FewSpecialFunctions.debye_function(15.0, 1.0, 2.4) ≈ 0.2661409156647294951955 atol=1e-15
+    @test FewSpecialFunctions.debye_function(20.0, 1.0, 1.24) ≈ 0.523361585088859680745 atol=1e-11
+    @test FewSpecialFunctions.debye_function(25.0, 1.0, 4.2) ≈ 0.07296496706218587 atol=1e-15
+    @test FewSpecialFunctions.debye_function(30.0, 1.0, 3.42) ≈ 0.1258426106590655660781 atol=1e-13
 
-    @test FewSpecialFunctions.Debye_function(3.0, 0.7) ≈ 0.761859 atol = 1e-5
-    @test FewSpecialFunctions.Debye_function(3.0, 2.8) ≈ 0.309995 atol = 1e-5
 end
